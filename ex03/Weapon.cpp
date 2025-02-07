a391f1eeb0e761b531e5e3c7459da3a284c79cc7
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-07 16:07:20 +0100

    change var name for clearer denote in ex03, start ex04 question to do: whar is Harl??

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 14:35:16 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/07 14:45:57 by ewu              ###   ########.fr       */
+/*   Updated: 2025/02/07 16:03:58 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,10 @@ const std::string& Weapon::getType() const
 }
 
 //set weapon type
-void Weapon::setType(std::string type)
+void Weapon::setType(std::string newType)
 {
-	this->type = type;
+	this->type = newType;
+//the weapon_type of current object (this->type) is replaced by newType passed by setType()
 }
 /**
  * THIS: a special ptr available in non-static member ft of a class
