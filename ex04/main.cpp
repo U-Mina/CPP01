2562fc0a6e36473534e1abd84c237cbe6c62f5ab
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-08 12:33:35 +0100

    ... A VERY EASY WAY WORKED!!!!

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 16:09:54 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/08 11:28:28 by ewu              ###   ########.fr       */
+/*   Updated: 2025/02/08 12:14:06 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ int main(int ac, char **av)
 	}
 	
 	//std read/open file part
-	std::ifstream fileName(filename);
-	if (!fileName.is_open())
+	std::ifstream inPut(filename);
+	if (!inPut.is_open())
 	{
 		std::cerr << "Fail to open: " << filename << std::endl;	
 		return 1;
@@ -49,43 +49,48 @@ int main(int ac, char **av)
 	}
 	
 	//searching and replacing part with std::getline()
-	std::string currentLine;
+	//std::string currentLine;
+	std::string newLine;
 	//while not reach EOF, read the <filename> line by line and store in currentLine
-	while (std::getline(fileName, currentLine))
+	while (std::getline(inPut, newLine))
 	{
-		size_t i = 0;
+		size_t pos = 0;
 		//search for matches until no more found in *currentLine*!!
 		while (1)
 		{
+			//std::string newLine = currentLine;
 			//finding position of s1, staring from 0 index
-			size_t pos = currentLine.find(s1, i);
+			size_t pos = newLine.find(s1, pos);
 			//npos indicates: not found or end of string or s1.length exceed left length, break
-			if (pos == std::string::npos || ((pos + s1.length()) > currentLine.length()))
+			if (pos == std::string::npos || ((pos + s1.length()) > newLine.length()))
 				break ;
-			
+			newLine.erase(pos, s1.length());
+			newLine.insert(pos, s2);
+			pos += s2.length();
 			//full match of string checking
-			bool matchFlag = true;
-			for (size_t index = 0; index < s1.length(); ++index)
-			{
-				if (currentLine[pos + index] != s1[index])
-				{
-					matchFlag = false;
-					break ;	
-				}
-			}
-			
-			if (matchFlag == true)
-			{
-				std::string frontLine = currentLine.substr(0, pos);
-				std::string backLine = currentLine.substr(i + s1.length());
-				currentLine = frontLine + s2 + backLine;//s1 is replaced and line combined
-				i = pos + s2.length();
-			}
-			else
-				i = pos + 1; 
+			// bool matchFlag = true;
+			// for (size_t index = 0; index < s1.length(); ++index)
+			// {
+			// 	if (currentLine[pos + index] != s1[index])
+			// 	{
+			// 		matchFlag = false;
+			// 		break ;	
+			// 	}
+			// }
+			// if (matchFlag == true)
+			// {
+			// 	std::string frontLine = currentLine.substr(0, pos);
+			// 	std::string backLine = currentLine.substr(i + s1.length());
+			// 	currentLine = frontLine + s2 + backLine;//s1 is replaced and line combined
+			// 	i = pos + s2.length();
+			// }
+			// else
+			// 	i = pos + 1; 
 		}
-		outPut << currentLine << "\n";
+		outPut << newLine << "\n";
 	}
+	inPut.close();
+	outPut.close();
 	return 0;
 }
 
